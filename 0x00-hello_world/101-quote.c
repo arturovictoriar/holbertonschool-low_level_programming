5725d822a2713e4c708ed8df63bd2f5d724f091d
arvicrin <1231@holbertonschool.com>
2019-09-18 21:21:06 +0000

    Prints to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-char *s= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 long l = 59;
 long fd = 1;
 long syscall = 1;
 long ret = 0;
-__asm__ ( "syscall"
+__asm__ ("syscall"
 : "=a" (ret)
 : "a" (syscall),
 "D" (fd),
