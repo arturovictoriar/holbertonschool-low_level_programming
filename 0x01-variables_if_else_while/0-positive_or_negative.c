588f08f5d8795a33bf7fbf3801d969d983914871
arvicrin <1231@holbertonschool.com>
2019-09-19 16:41:58 +0000

    Assign a random number and print it is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n==0)
+if (n == 0)
 printf("%d is zero\n", n);
 else
 {
-if (n>0)
+if (n > 0)
 printf("%d is positive\n", n);
 else
 {
-if (n<0)
+if (n < 0)
 printf("%d is negative\n", n);
 }
 }
