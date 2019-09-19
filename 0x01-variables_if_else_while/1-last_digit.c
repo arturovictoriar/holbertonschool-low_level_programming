667c33bc97053f8d3815b5e08f00379ce3151a31
arvicrin <1231@holbertonschool.com>
2019-09-19 16:44:28 +0000

    Assign a random number and print the last digit of the number stored

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,10 +10,10 @@ int n, lastdigit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-lastdigit=n%10;
-if (lastdigit<6)
+lastdigit = n % 10;
+if (lastdigit < 6)
 {
-if (lastdigit==0)
+if (lastdigit == 0)
 printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 else
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
