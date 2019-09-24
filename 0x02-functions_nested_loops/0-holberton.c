5cb51b0b07f6888deb22a1306f652b81754b73bf
arvicrin <1231@holbertonschool.com>
2019-09-24 00:20:54 +0000

    Create and use a putchar funtion

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char Holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+int i;
+for (i = 0; i < 9; i++)
+_putchar(Holberton[i]);
+_putchar('\n');
+return (0);
+}
