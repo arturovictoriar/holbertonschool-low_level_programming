aef2d5c6e42a2c906c45a61ad8d980ccb181c8d7
arvicrin <1231@holbertonschool.com>
2019-09-24 21:21:13 +0000

    new

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+long int num1 = 0, num2 = 1, num3 = 0, sum;
+for (i = 0; i < 40; i++)
+{
+num3 = num2 + num1;
+num1 = num2;
+num2 = num3;
+if (num3 <= 4000000)
+sum = sum + num3;
+}
+printf("%ld\n", sum);
+return (0);
+}
