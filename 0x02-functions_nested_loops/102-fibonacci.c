839891b93258b7fca1ccff97b10329449b73b39c
arvicrin <1231@holbertonschool.com>
2019-09-24 21:11:18 +0000

    new

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,18 @@
 int main(void)
 {
 int i;
-long int num1 = 1, num2 = 1, num3 = 1;
+long int num1 = 0, num2 = 1, num3 = 0;
 for (i = 0; i < 50; i++)
 {
-printf("%ld\n", num3);
+
 num3 = num2 + num1;
 num1 = num2;
 num2 = num3;
+if (i != 49)
+printf("%ld, ", num3);
+else
+printf("%ld", num3);
 }
+printf("\n");
 return (0);
 }
