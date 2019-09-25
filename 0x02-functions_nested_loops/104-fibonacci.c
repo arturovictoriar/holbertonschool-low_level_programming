da41b30bb982de45b999e88d22c566c883c77cd8
arvicrin <1231@holbertonschool.com>
2019-09-25 04:18:46 +0000

    new

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,50 +8,37 @@
 int main(void)
 {
 int i;
-unsigned long int fibo = 0, fiboa = 0, fibob =0;
-unsigned long int dos = 1, uno = 0, dosa = 0,  dosb =0, unoa = 0, unob = 0;
+unsigned long int fibo = 0, fiboa = 0, fibob = 0;
+unsigned long int dos = 1, uno = 0, dosa = 0,  dosb = 0, unoa = 0, unob = 0;
 
 for (i = 0; i < 91; i++)
 {
 fibo = dos + uno;
 printf("%ld, ", fibo);
-
 uno = dos;
 dos = fibo;
 }
-
 dosa = dos / 10000000000;
 unoa = uno / 10000000000;
-
 dosb = dos % 10000000000;
 unob = uno % 10000000000;
-
-
 for (i = 92; i < 98; i++)
 {
-
 fiboa = dosa + unoa;
 fibob = dosb + unob;
-
 if (fibob > 10000000000)
 {
 fiboa += 1;
 fibob %= 10000000000;
 }
-
 printf("%ld%ld", fiboa, fibob);
-
 unoa = dosa;
 unob = dosb;
 dosa = fiboa;
 dosb = fibob;
-
 if (i != 97)
 printf(", ");
 }
-
-
-
 printf("\n");
 return (0);
 }
