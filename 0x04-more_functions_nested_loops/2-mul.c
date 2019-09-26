82f4a6e121c87d2974e6dca4b37f743ff70774ef
arvicrin <1231@holbertonschool.com>
2019-09-26 14:48:10 +0000

    Multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ * @a: is a int variable
+ * @b: is a int variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
