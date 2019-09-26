37e9c4cdda4efe9eab95bc689a684281fd5b8e67
arvicrin <1231@holbertonschool.com>
2019-09-26 15:31:45 +0000

    Draw a diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - check the code for Holberton School students.
+ * @n: is a int variable
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+
+for (j = 0; j < i; j++)
+_putchar(' ');
+
+_putchar('\\');
+_putchar('\n');
+}
+}
+
+else
+_putchar('\n');
+}
