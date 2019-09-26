01c239b651a8107cb93cf00c7d03010849f3e8ea
arvicrin <1231@holbertonschool.com>
2019-09-26 17:03:00 +0000

    Print 10 times the numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,11 @@ for (i = 0; i < 10; i++)
 
 for (num = 0; num <= 14; num++)
 {
-if (num < 10)
-_putchar(num + '0');
-else
-{
+if (num > 9)
 _putchar((num / 10) + '0');
+
 _putchar((num % 10) + '0');
-}
+
 }
 _putchar('\n');
 }
