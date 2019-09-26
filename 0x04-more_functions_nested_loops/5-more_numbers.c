403ae8d5c5e65c26f4374d42cca8cc06f248ee78
arvicrin <1231@holbertonschool.com>
2019-09-26 15:12:49 +0000

    Print 10 times the numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int i, num;
+for (i = 0; i < 10; i++)
+{
+
+for (num = 0; num <= 14; num++)
+{
+if (num < 10)
+_putchar(num + '0');
+else
+{
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
