ce4bf9abb4fcd1672204629712c718f1be21a93e
arvicrin <1231@holbertonschool.com>
2019-09-26 19:38:59 +0000

    Print an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_number - check the code for Holberton School students.
+ * @n: is a int number
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+
+if (n < 0)
+{
+n *= -1;
+
+_putchar('-');
+
+if (n > 9)
+_putchar(((n / 10) % 10) + '0');
+
+_putchar(n % 10 + '0');
+
+}
+else
+{
+if (n > 999)
+_putchar((((n / 10) / 10) / 10) + '0');
+
+if (n > 99)
+_putchar((((n / 10) / 10) % 10) + '0');
+
+if (n > 9)
+_putchar(((n / 10) % 10) + '0');
+
+_putchar(n % 10 + '0');
+}
+}
