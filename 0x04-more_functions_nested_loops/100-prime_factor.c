b57c6393bd417085408cacc8c885975a89b58e0c
arvicrin <1231@holbertonschool.com>
2019-09-26 19:03:28 +0000

    Largest prime factor of the number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,14 +10,14 @@ int main(void)
 
 unsigned long i, prime, n = 612852475143;
 
-for (i = 3; i <= sqrt(n); i += 2)
+for (i = 3; i <= n; i += 2)
 {
-while (n % i == 0) { 
-            prime = i; 
-            n = n / i; 
-        } 
+while (n % i == 0)
+{
+prime = i;
+n = n / i;
 }
-
+}
 printf("%lu\n", prime);
 return (0);
 }
