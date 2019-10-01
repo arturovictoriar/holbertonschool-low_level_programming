e894ab8e44b5679128bcc8070461a29105e6ce68
arvicrin <1231@holbertonschool.com>
2019-10-01 00:42:31 +0000

    New

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * swap_int - check the code for Holberton School students.
+ * @*a: is a pointer int type
+ * @*b: is a pointer int type
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+
+
+*a = *b;
+*b = c;
+
+
+}
