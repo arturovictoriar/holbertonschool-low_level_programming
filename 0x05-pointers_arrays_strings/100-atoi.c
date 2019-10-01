af1ab2fb42bd35bfb2dd53b3e713ed9325d6361f
arvicrin <1231@holbertonschool.com>
2019-10-01 21:52:07 +0000

    string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,16 +5,15 @@
  * @s: pointer char type
  * Return: Always 0.
  */
-
 int _atoi(char *s)
 {
-int i = 0, dig = 0, digi = 1, check = 0, negative = 0, numero = 0, nume, out = 0;
+int i = 0, dig = 0, digi = 1, check = 0;
+int negative = 0, numero = 0, nume, out = 0;
 char num;
 for (i = 0; *s != '\0'; i++)
 {
 if (*s == '-')
 negative++;
-
 for (num = '0'; num <= '9'; num++)
 {
 if (*s == num)
@@ -25,41 +24,28 @@ dig++;
 else
 out++;
 }
-
 if (out == 10 && check == 1)
 break;
 s++;
 out = 0;
 }
-
 dig--;
-
 s -= i;
-
 for (i = 0; i < dig; i++)
 digi *= 10;
-
-
 while (*s != '\0')
 {
-
 for (nume = 0; nume <= 9; nume++)
 {
 if (*s == (nume + '0'))
 {
 numero += (nume * digi);
 digi /= 10;
-
 }
 }
-
 s++;
 }
-
-
-
 if ((negative % 2) != 0)
 numero *= -1;
-
 return (numero);
 }
