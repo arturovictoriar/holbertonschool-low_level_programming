affb3c1adfbd21601dc8a781d2cd12d0fa84dc13
arvicrin <1231@holbertonschool.com>
2019-10-02 04:22:01 +0000

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,44 +7,19 @@
  */
 int _atoi(char *s)
 {
-int i = 0, dig = 0, digi = 1, check = 0;
-int negative = 0, numero = 0, nume, out = 0;
-char num;
-for (i = 0; *s != '\0'; i++)
+int negative = 1, i = 0, numero = 0;
+
+for (i = 0; s[i] != '\0'; i++)
 {
-if (*s == '-')
-negative++;
-for (num = '0'; num <= '9'; num++)
-{
-if (*s == num)
-{
-check = 1;
-dig++;
-}
-else
-out++;
-}
-if (out == 10 && check == 1)
+if (s[i] == '-')
+negative *= -1;
+
+else if (s[i] >= 0 + '0' && s[i] < 10 + '0')
+numero = numero * 10 + s[i] - '0';
+
+else if (s[i - 1] >= 0 + '0' && s[i - 1] < 10 + '0')
 break;
-s++;
-out = 0;
 }
-s -= i;
-for (i = 0; i < dig - 1; i++)
-digi *= 10;
-while (*s != '\0')
-{
-for (nume = 0; nume <= 9; nume++)
-{
-if (*s == (nume + '0'))
-{
-numero += (nume * digi);
-digi /= 10;
-}
-}
-s++;
-}
-if ((negative % 2) != 0)
-numero *= -1;
-return (numero);
+
+return (numero *negative);
 }
