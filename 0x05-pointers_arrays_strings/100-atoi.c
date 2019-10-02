3722855f1dbdbf1d2361aff4296a28a48f5fae6a
arvicrin <1231@holbertonschool.com>
2019-10-02 04:29:46 +0000

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,8 @@
  */
 int _atoi(char *s)
 {
-int negative = 1, i = 0, numero = 0;
+int negative = 1, i = 0;
+unsigned int numero = 0;
 
 for (i = 0; s[i] != '\0'; i++)
 {
@@ -15,7 +16,7 @@ if (s[i] == '-')
 negative *= -1;
 
 else if (s[i] >= 0 + '0' && s[i] < 10 + '0')
-numero = numero * 10 + s[i] - '0';
+numero = numero * 10 + (s[i] - '0');
 
 else if (s[i - 1] >= 0 + '0' && s[i - 1] < 10 + '0')
 break;
