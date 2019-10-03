d6fb7175ba0bef485aa5548ca881dc45b5bfed49
arvicrin <1231@holbertonschool.com>
2019-10-03 21:23:59 +0000

    Capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * cap_string - check the code for Holberton School students.
+ * @a: is a pointer type char
+ * 
+ * Return: Always 0.
+ *
+ */
+char *cap_string(char *a)
+{
+char separator[] = " \t\n,;.!?\"()}"; 
+char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i, k, m;
+char j;
+
+for (i = 0; a[i] != '\0'; i++)
+{
+
+for (m = 0; separator[m] != '\0'; m++)
+{
+
+if (a[i - 1] == separator[m])
+{
+k = 0;
+
+for (j = 'a'; j <= 'z'; j++, k++)
+{
+if (a[i] == j)
+a[i] = l[k];
+}
+
+}
+
+}
+
+
+}
+return (a);
+}
