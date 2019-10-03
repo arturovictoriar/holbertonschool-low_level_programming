d0c89c265d9d0935127130ce73bb07bf269f347a
arvicrin <1231@holbertonschool.com>
2019-10-03 18:09:31 +0000

    Copie a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncat - check the code for Holberton School students.
+ * @dest: is a pointer type char
+ * @src: is a pointer type char
+ * @n: is a int variable
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+
+for ( ; i < n; i++)
+dest[i] = '\0';
+
+return (dest);
+}
