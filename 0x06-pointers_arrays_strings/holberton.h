de6b2cd2cf89884a01faa2f13444571c9a0326e2
arvicrin <1231@holbertonschool.com>
2019-10-03 17:50:13 +0000

    Hearders

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_FUNTIONS
+#define HOLBERTON_FUNTIONS
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);  /* An example function declaration */
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+#endif
