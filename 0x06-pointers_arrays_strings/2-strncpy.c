19e19df2b2fe817da6a19273bb2107db1062fe1f
arvicrin <1231@holbertonschool.com>
2019-10-03 18:11:09 +0000

    Copie a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strncat - check the code for Holberton School students.
+ * _strncpy - check the code for Holberton School students.
  * @dest: is a pointer type char
  * @src: is a pointer type char
  * @n: is a int variable
