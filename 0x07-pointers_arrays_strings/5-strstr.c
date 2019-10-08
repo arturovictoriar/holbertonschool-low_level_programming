7fc7dd38d94b6019d241eb3e086610a67358d940
arvicrin <1231@holbertonschool.com>
2019-10-08 04:26:59 +0000

    Locate a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,14 +9,13 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i, num, cont;
+int i, num = 0, cont;
 
 for (cont = 0; needle[cont] != '\0'; cont++)
 ;
 
 for (; *haystack != '\0'; haystack++)
 {
-num = 0;
 if (*haystack == needle[0])
 {
 num++;
@@ -24,15 +23,15 @@ haystack++;
 for (i = 1; i < cont; i++, haystack++)
 {
 if (*haystack == needle[i])
-{
 num++;
-}
+
 else
 {
 haystack--;
 num = 0;
 break;
 }
+
 }
 }
 
