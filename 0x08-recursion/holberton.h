a6471955fa8b3e45723e2c4d3aaa1f0679a5918c
arvicrin <1231@holbertonschool.com>
2019-10-09 18:08:17 +0000

    Header

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_FUNTIONS
+#define HOLBERTON_FUNTIONS
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);  /* An example function declaration */
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+#endif
