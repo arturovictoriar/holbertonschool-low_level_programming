2cae3b1ce1088def9ff80c18697661c5500043f9
arvicrin <1231@holbertonschool.com>
2019-10-10 03:43:06 +0000

    Return 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,17 +2,24 @@
 #include <stdio.h>
 int fakefake(int prime, int primediv);
 /**
- * is_prime_number - get the value of x raised to power of y
+ * is_prime_number - get prime number of given number
  * @n: is a natural number
  * Return: 1 if n is a prime number and 0 if not.
  */
 
 int is_prime_number(int n)
 {
-return (fakefake(n , 3));
+return (fakefake(n, 3));
 }
 
-int fakefake (int prime, int primediv)
+/**
+ * fakefake - get the prime number
+ * @prime: is a natural number
+ * @primediv: is a natural number
+ * Return: 1 if n is a prime number and 0 if not.
+ */
+
+int fakefake(int prime, int primediv)
 {
 if (prime < 0)
 return (0);
@@ -20,10 +27,10 @@ return (0);
 else if (prime == 1)
 return (0);
 
-else if (prime % primediv == 0 && primediv <= prime/2)
-return(0);
+else if (prime % primediv == 0 && primediv <= prime / 2)
+return (0);
 
-else if (prime % primediv != 0 && primediv > prime/2)
+else if (prime % primediv != 0 && primediv > prime / 2)
 return (1);
 
 else
