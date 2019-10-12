8dbe44b5fd28f3fb7caa1fd1a44ef5db987401a3
arvicrin <1231@holbertonschool.com>
2019-10-12 18:26:08 +0000

    Add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int power(int b);
 /**
   * main - print the multiplication of two arguments
   * @argc: has the length of the arguments
@@ -10,64 +9,30 @@ int power(int b);
   */
 int main(int argc, char **argv)
 {
+	int i, j, num = 0;
 
-	int i, j, cont, num[500], numero = 0, flag = 0, conta;
-
-	for (i = 0; i < 500; i++)
-		num[i] = 0;
 	if (argc < 3)
 		printf("0\n");
+
 	else
 	{
-		for (i = 1; argv[i] != '\0'; i++)
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; (argv[i])[j] != '\0'; j++)
 		{
-			for (cont = 0; (argv[i])[cont] != '\0'; cont++)
-				;
-			for (conta = 1; argv[conta + 1] != '\0'; conta++)
-				;
-			for (j = 0, cont--; (argv[i])[j] != '\0'; j++)
+		
+			if (((argv[i])[j] - '0') >= 0 && ((argv[i])[j] - '0') <=9)
+				continue;
+			else
 			{
-				if (((argv[i])[cont - j] - '0') >= 0 && ((argv[i])[cont - j] - '0') <= 9)
-					num[i - 1] += power(j) * ((argv[i])[cont - j] - '0');
-				else
-				{
-				flag = 1;
-				break;
-				}
+				printf("Error\n");
+				return (1);
 			}
-			if (flag == 1)
-				break;
 		}
-		if (flag == 1)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			for (i = 0; i < conta; i++)
-				numero += num[i];
-
-			printf("%d\n", numero);
-		}
+		num += atoi(argv[i]);
+	}
+	printf("%d\n", num);
 	}
 	return (0);
 }
-
-
-
-/**
-  * power - get the power 10 times b
-  * @b: is the times of 10
-  * Return: the power 10 times b
-  */
-
-int power(int b)
-{
-
-	int i, num = 1;
-
-	for (i = 0; i < b; i++)
-		num *= 10;
-	return (num);
-}
+		
