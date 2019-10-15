18c10090004de09d9ce6d8ea1b334df968e5ed23
arvicrin <1231@holbertonschool.com>
2019-10-15 23:28:54 +0000

    Concatenates all the arguments of your program

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -17,7 +17,7 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			cont++;
