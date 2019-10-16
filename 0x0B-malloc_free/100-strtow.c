5b6b6ad7e8fda6eb7952dd9b6df9d63c90c4834d
arvicrin <1231@holbertonschool.com>
2019-10-16 04:18:26 +0000

    Split a string into words

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+char **spll(char *str, int cont1);
 /**
   * strtow - splits a string into words
   * @str: a string with spaces
@@ -10,8 +10,8 @@
 
 char **strtow(char *str)
 {
-	int i, j , k, cont1, cont2, flag1 = 0;
-	char **splitt;
+	int i, cont1, flag1 = 0;
+
 	if (str == NULL || str == '\0')
 	return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
@@ -24,14 +24,25 @@ char **strtow(char *str)
 		else if (flag1 == 1 && str[i + 1] == ' ')
 			flag1 = 0;
 	}
-	flag1 = 0;
+	return (spll(str, cont1));
+}
+
+/**
+  * spll - splits a string into words
+  * @str: a string with spaces
+  * @cont1: is a counter
+  * Return: an address
+  */
+
+char **spll(char *str, int cont1)
+{
+	int i, j, k, cont2, flag1 = 0;
+	char **splitt;
 
 	splitt = malloc((cont1 + 1) * sizeof(char *));
-
 	if (!splitt)
 		return (NULL);
-
-	for (i = 0, j = 0; i < cont1; i++, j++)
+	for (i = 0, j = 0, flag1 = 0, cont2 = 0; i < cont1; i++, j++)
 	{
 		for ( ; str[j] != '\0'; j++)
 		{
@@ -40,23 +51,22 @@ char **strtow(char *str)
 				cont2++;
 				flag1 = 1;
 			}
-
 			else if (flag1 == 1 && (str[j + 1] == ' ' || str[j] != '\0'))
 			{
-				 flag1 = 0;
-				 splitt[i] = malloc(cont2 * sizeof(char) + 1);
-				 if (splitt[i] == NULL)
-				 {
-					 free(splitt);
-					 for (i--; i >= cont1; i--)
-						 free(splitt[i]);
-					 return (NULL);
-				 }
-				 for (k = 0; k < cont2; k++)
-					 splitt[i][k] = str[(j - cont2) + k];
-				 splitt[i][k] = '\0';
-				 cont2 = 0;
-				 break;
+				flag1 = 0;
+				splitt[i] = malloc(cont2 * sizeof(char) + 1);
+				if (splitt[i] == NULL)
+				{
+					free(splitt);
+					for (i--; i >= cont1; i--)
+						free(splitt[i]);
+					return (NULL);
+				}
+				for (k = 0; k < cont2; k++)
+					splitt[i][k] = str[(j - cont2) + k];
+				splitt[i][k] = '\0';
+				cont2 = 0;
+				break;
 			}
 		}
 	}
