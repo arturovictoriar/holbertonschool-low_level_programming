188d06c5fc489e103473c17c564218e5fc129523
arvicrin <1231@holbertonschool.com>
2019-10-17 20:17:57 +0000

    Reallocate a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -39,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (neww == NULL)
 		return (NULL);
 
-	for (i = 0; i < new_size; i++)
+	for (i = 0; i < old_size; i++)
 		neww[i] = replace[i];
 
 	free(ptr);
