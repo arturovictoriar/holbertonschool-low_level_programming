60b59c174bc4b6e4d20d6d074aa04558cc6c0be0
arvicrin <1231@holbertonschool.com>
2019-10-21 19:16:58 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MYDOG
+#define MYDOG
+
+/**
+  * struct dog - Short description
+  * @name: First member
+  * @age: Second member
+  * @owner: Third member
+  *
+  * Description: Longer description
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* My dog */
