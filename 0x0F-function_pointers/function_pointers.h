28336334aea6eafbb4eab332b897082df963fe9f
arvicrin <1231@holbertonschool.com>
2019-10-23 17:32:21 +0000

    Header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
 #ifndef POINTERS_FUNTIONS
 #define POINTERS_FUNTIONS
-#include <stddef.h>
 int _putchar(char c);  /* An example function declaration */
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
