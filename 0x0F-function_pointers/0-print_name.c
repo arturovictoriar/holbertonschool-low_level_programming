a0d1872045084ab218dc037b7e1ef7fbdc3ad3df
arvicrin <1231@holbertonschool.com>
2019-10-23 17:12:06 +0000

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * print_name - fills memory with a constant byte
+  * @name: is the name of the dog
+  * @f: is a funtion pointer
+  * Return: a void pointer
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
