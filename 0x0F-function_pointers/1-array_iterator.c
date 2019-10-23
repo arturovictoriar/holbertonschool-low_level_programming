217a6054eafa7ec328e03a79fcf9f6603ff483b2
arvicrin <1231@holbertonschool.com>
2019-10-23 17:41:06 +0000

    Fix 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,8 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	if (array != NULL && action != NULL && size != 0)
-	for (i = 0; i < size; i++)
-		action(array[i]);
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
