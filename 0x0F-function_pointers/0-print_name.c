ce24a6ba03fcd39fed719519ba52c8d0ea03dc3b
arvicrin <1231@holbertonschool.com>
2019-10-23 19:46:38 +0000

    Fix 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
   * print_name - fills memory with a constant byte
@@ -12,5 +10,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	(*f)(name);
+	f(name);
 }
