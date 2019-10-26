b17e5dc38a31fb8a49e1dae9c747ee6cccfc227b
arvicrin <1231@holbertonschool.com>
2019-10-26 01:55:49 +0000

    Print Hello, Holberton, followed by a new line with assembly

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data				;.data main here
+msg db "Hello Holberton", 10		;String gets initialized
+l equ $-msg				;Length Of String
+
+section .text				;.text starts here
+
+global main				;Moving to main
+
+main:					;_start label
+mov rax,1				;Sys_Write Function
+mov rdi,1				;Std_Out File Descriptor
+mov rsi,msg				;Offset of msg
+mov rdx,l				;Length Of msg
+syscall					;Call the Kernel
+mov rax,60				;Sys_Exit Function
+mov rdi,0				;Sucessful Termination
+
+syscall					;Call The Kernel
+end:					;end Label
