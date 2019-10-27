33333e0f3b5da0c2d4fbbbb7209a6da1c59b6227
arvicrin <1231@holbertonschool.com>
2019-10-27 02:46:13 +0000

    Fix 4

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,43 +5,50 @@
   * @a: is string
   * Return: nothing
   */
+
 void ch(va_list a)
 {
 	printf("%c", va_arg(a, int));
 }
+
 /**
   * in - fills memory with a constant byte
   * @a: is string
   * Return: nothing
   */
+
 void in(va_list a)
 {
 	printf("%d", va_arg(a, int));
 }
+
 /**
   * fl - fills memory with a constant byte
   * @a: is string
   * Return: nothing
   */
+
 void fl(va_list a)
 {
 	printf("%f", va_arg(a, double));
 }
+
 /**
   * cha - fills memory with a constant byte
   * @a: is string
   * Return: nothing
   */
+
 void cha(va_list a)
 {
 	char *aa = va_arg(a, char *);
 
-	if (aa == NULL)
+	if (aa != NULL)
 	{
-		printf("nil");
+		printf("%s", aa);
 		return;
 	}
-	printf("%s", aa);
+	printf("(nil)");
 }
 
 /**
@@ -55,7 +62,7 @@ void print_all(const char * const format, ...)
 	va_list gett;
 	unsigned int i, j;
 	char *separ = "";
-	op_t  o[] = {
+	op_t o[] = {
 		{"c", ch},
 		{"i", in},
 		{"f", fl},
@@ -70,7 +77,7 @@ void print_all(const char * const format, ...)
 		j = 0;
 		while (o[j].op != NULL)
 		{
-			if (format[i] == (o[j].op)[0])
+			if (format[i] == o[j].op[0])
 			{
 				printf("%s", separ);
 				o[j].f(gett);
