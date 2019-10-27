b339f1ba433bc783aa0e9fd4b3856a12944a0d92
arvicrin <1231@holbertonschool.com>
2019-10-27 03:26:15 +0000

    Print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,51 @@
 #include "variadic_functions.h"
 
+void ch(va_list a);
+void in(va_list a);
+void fl(va_list a);
+void cha(va_list a);
+
+/**
+  * print_all - fills memory with a constant byte
+  * @format: is string
+  * Return: nothing
+  */
+
+void print_all(const char * const format, ...)
+{
+	va_list gett;
+	unsigned int i, j;
+	char *separ = "";
+	op_t o[] = {
+		{"c", ch},
+		{"i", in},
+		{"f", fl},
+		{"s", cha},
+		{NULL, NULL}
+	};
+
+	va_start(gett, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		j = 0;
+		while (o[j].op != NULL)
+		{
+			if (format[i] == o[j].op[0])
+			{
+				printf("%s", separ);
+				o[j].f(gett);
+				separ = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(gett);
+	printf("\n");
+}
+
 /**
   * ch - fills memory with a constant byte
   * @a: is string
@@ -43,51 +89,10 @@ void cha(va_list a)
 {
 	char *aa = va_arg(a, char *);
 
-	if (aa != NULL)
+	if (aa == NULL)
 	{
-		printf("%s", aa);
+		printf("(nil)");
 		return;
 	}
-	printf("(nil)");
-}
-
-/**
-  * print_all - fills memory with a constant byte
-  * @format: is string
-  * Return: nothing
-  */
-
-void print_all(const char * const format, ...)
-{
-	va_list gett;
-	unsigned int i, j;
-	char *separ = "";
-	op_t o[] = {
-		{"c", ch},
-		{"i", in},
-		{"f", fl},
-		{"s", cha},
-		{NULL, NULL}
-	};
-
-	va_start(gett, format);
-	i = 0;
-	while (format != NULL && format[i] != '\0')
-	{
-		j = 0;
-		while (o[j].op != NULL)
-		{
-			if (format[i] == o[j].op[0])
-			{
-				printf("%s", separ);
-				o[j].f(gett);
-				separ = ", ";
-				break;
-			}
-			j++;
-		}
-		i++;
-	}
-	va_end(gett);
-	printf("\n");
+	printf("%s", aa);
 }
