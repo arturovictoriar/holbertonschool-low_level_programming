9b6750914602a5f9d2ac364caa82792db99f0939
arvicrin <1231@holbertonschool.com>
2019-10-31 15:53:02 +0000

    Print all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * print_list - fills memory with a constant byte
+  * @h: is the owner of the dog
+  * Return: a number
+  */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+	for(i = 0; h != NULL; i++)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str); 
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", 0, "(nil)"); 
+			h = h->next;
+		}
+	}
+	return (i);
+}
