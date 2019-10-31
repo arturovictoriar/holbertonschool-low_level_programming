521448184210ad833947bd1617db8517a2aca784
arvicrin <1231@holbertonschool.com>
2019-10-31 19:18:06 +0000

    Fix 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	if (str == NULL)
 	{
-		new->str = "(nil)";
+		new->str = strdup("(nil)");
 		new->len = 0;
 		new->next = *head;
 		*head = new;
