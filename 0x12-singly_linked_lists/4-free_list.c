ed7b16f4e8e4c911266c4739ad4c28829c6bda20
arvicrin <1231@holbertonschool.com>
2019-11-01 00:37:25 +0000

    Fix3

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,15 +2,20 @@
 
 /**
   * free_list - fills memory with a constant byte
-  * @h: is the owner of the dog
+  * @head: is the owner of the dog
   * Return: a number
   */
 void free_list(list_t *head)
 {
-	if (head != NULL)
+	list_t *savepoin;
+
+	while (head->next != NULL)
 	{
-		free_list(head->next);
+		savepoin = head->next;
 		free(head->str);
+		free(head);
+		head = savepoin;
 	}
+	free(head->str);
 	free(head);
 }
