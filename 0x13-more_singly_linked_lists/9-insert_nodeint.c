1d75c4bf72208bf2e0b77bd288298b26d5b8d31f
arvicrin <1231@holbertonschool.com>
2019-11-04 16:44:01 +0000

    Insert a new node at a given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+  * insert_nodeint_at_index - fills memory with a constant byte
+  * @head: is the owner of the dog
+  * @idx: is the index
+  * @n: is a number
+  * Return: a address
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *headcopy;
+	unsigned int i;
+
+	headcopy = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*head == NULL)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		for (i = 0; (i < idx - 1) && (headcopy != NULL); i++)
+			headcopy = headcopy->next;
+		if (headcopy != NULL)
+		{
+			new->next = (headcopy)->next;
+			(headcopy)->next = new;
+		}
+		else
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+
+	return (headcopy);
+}
