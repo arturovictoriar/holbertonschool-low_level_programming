57dd2b4421d27ee6dc0699e1df962a1994d67b8a
arvicrin <1231@holbertonschool.com>
2019-11-13 01:38:35 +0000

    Elf Header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * main - fills memory with a constant byte
+  * @argc: is the size of the pointer
+  * @argv: a index
+  * Return: a int
+  */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+	return (0);
+}
