cc328fdeffc70e8b5c0f97b501f975c4330f1486
arvicrin <1231@holbertonschool.com>
2019-12-13 20:33:16 +0000

    Insert a new node at a given position, fix index out of range

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,19 +9,15 @@
   */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *headcopy;
+	dlistint_t *new, *headcopy = *h;
 	unsigned int i;
-
-	headcopy = *h;
-	if (headcopy == NULL)
-		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 
-	while (headcopy->prev != NULL)
+	while (headcopy != NULL && headcopy->prev != NULL)
 	{
 		headcopy = headcopy->prev;
 		*h = (*h)->prev;
@@ -35,7 +31,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	for (i = 0; (i < idx - 1); i++)
+	{
+		if (headcopy == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
 		headcopy = headcopy->next;
+		if (headcopy == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
 
 	new->next = headcopy->next;
 	headcopy->next = new;
