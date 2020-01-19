a2b96e21d1591d8ccd73d10854ed419ca5226fa2
arvicrin <1231@holbertonschool.com>
2020-01-19 04:31:21 +0000

    Function that adds an element to the hash table.

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,76 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_set - adds an element to the hash table
+  * @value: value associated with the key
+  * @key: is the key
+  * @ht: hash table you want to add or update the key/value
+  * Return: 1 in success 0 in otherwise
+  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node = NULL, *list = NULL;
+	unsigned long int index = 0, size = ht->size;
+	char *value_copy = NULL;
+
+	if (key == NULL || value == NULL || ht == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((const unsigned char *) key, size);
+	list = (ht->array)[index];
+
+	if (list != NULL)
+	{
+		while (list)
+		{
+			if (!strcmp(list->key, key))
+			{
+				value_copy = strdup(value);
+				if (value_copy == NULL)
+					return (0);
+				free(list->value);
+				list->value = value_copy;
+				return (1);
+			}
+			list = list->next;
+		}
+	}
+
+	node = h_n_create(key, value);
+	if (node == NULL)
+		return (0);
+
+	node->next = ht->array[index];
+	(ht->array)[index] = node;
+
+	return (1);
+}
+
+/**
+  * h_n_create - create a hash node
+  * @key:  is the key name
+  * @value: value associated with the key
+  * Return: the position memory where is stored the head
+  */
+
+hash_node_t *h_n_create(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->key = (char *) key;
+
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = NULL;
+
+	return (new);
+}
