f75803c3419041dd387465f1708762f80b517a2f
arvicrin <1231@holbertonschool.com>
2020-01-19 05:07:59 +0000

    Function that adds an element to the hash table., fix condition key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = NULL, *list = NULL;
 	unsigned long int index = 0, size = ht->size;
 
-	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *) key, size);
