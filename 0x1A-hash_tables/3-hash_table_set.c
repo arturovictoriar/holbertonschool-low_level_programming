c0b8f29eed95cca18d1bfaba474dcaa97fa14f80
arvicrin <1231@holbertonschool.com>
2020-01-19 05:45:25 +0000

    Function that adds an element to the hash table., Fix name, come back to edd71ee

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,36 +12,63 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = NULL, *list = NULL;
 	unsigned long int idx = 0;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (key == NULL || value == NULL || ht == NULL || *key == '\0')
 		return (0);
 
 	idx = key_index((const unsigned char *) key, ht->size);
-	list = ht->array[idx];
+	list = (ht->array)[idx];
 
-	while (list)
+	if (list != NULL)
 	{
-		if (strcmp(list->key, key) == 0)
+		while (list)
 		{
-			free(list->value);
-			list->value = strdup(value);
-			if (list->value == NULL)
-				return (0);
-			return (1);
+			if (!strcmp(list->key, key))
+			{
+				free(list->value);
+				list->value = strdup(value);
+				if (list->value == NULL)
+					return (0);
+				return (1);
+			}
+			list = list->next;
 		}
-		list = list->next;
 	}
 
-	node = malloc(sizeof(hash_node_t));
+	node = h_n_create(key, value);
 	if (node == NULL)
 		return (0);
 
-	node->key = strdup(key);
-	node->value = strdup(value);
-	if (node->key == NULL || node->value == NULL)
-		return (0);
-
-	node->next = ht->array[idx];
-	ht->array[idx] = node;
+	node->next = (ht->array)[idx];
+	(ht->array)[idx] = node;
 
 	return (1);
+}
+
+/**
+  * h_n_create - create a hash node
+  * @key:  is the key name
+  * @value: value associated with the key
+  * Return: the position memory where is stored the head
+  */
+
+hash_node_t *h_n_create(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->key = strdup(key);
+
+	new->value = strdup(value);
+	if (new->value == NULL || new->key == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = NULL;
+
+	return (new);
 }
