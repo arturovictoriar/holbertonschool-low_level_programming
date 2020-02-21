ecc87bf7a8cdeabfbc370643e85f0201ce4c75c4
arvicrin <1231@holbertonschool.com>
2020-02-21 21:20:26 +0000

    Fix condition

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,7 +16,7 @@ def island_perimeter(grid):
         for j, cell in enumerate(g_cell):
             if cell:
                 perimeter += 4
-                if i != 0 and j + 1 != len(g_cell) and grid[i - 1][j] == 1:
+                if i != 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
                 if j + 1 != len(g_cell) and g_cell[j + 1] == 1:
                     perimeter -= 2
