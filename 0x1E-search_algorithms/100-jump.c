1c159eb3ddff09f1485b890a056963df11daf74c
arvicrin <1231@holbertonschool.com>
2020-04-16 17:34:22 +0000

    Jump search another method

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,50 +2,33 @@
 #include <math.h>
 
 /**
-  * min_value - get the min value of two numbers
-  * @l: number l
-  * @r: number r
-  * Return: return the minimun value
-  */
-size_t min_value(size_t l, size_t r)
-{
-	return ((l < r) ? l : r);
-}
-
-/**
  * jump_search - search a value in a sort list
  * @array: list of numbers
  * @size: lenght of the list
- * @value: value to search for
+ * @value: value to search
  * Return: the first index of value otherwise -1
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t a = 0, b = sqrt(size), step = sqrt(size);
+	size_t a = 0, step = sqrt(size);
 
 	if (array == NULL)
 		return (-1);
-	while (array[min_value(b, size) - 1] < value)
+	while (array[a] < value)
 	{
 		printf("Value checked array[%d] = [%d]\n", (int)a, array[a]);
-		a = b;
-		b += step;
+		a += step;
 		if (a >= size)
 			break;
 	}
-	if ((int)(a - step) >= 0)
-	{
-		a -= step;
-		b -= step;
-	}
-	else
-		printf("Value checked array[%d] = [%d]\n", (int)a, array[a]);
-	printf("Value found between indexes [%d] and [%d]\n", (int)a, (int)b);
+	a -= step;
+	printf("Value found between indexes [%d] and [%d]\n",
+					(int)a, (int)(a + step));
 	while (array[a] < value)
 	{
 		printf("Value checked array[%d] = [%d]\n", (int)a, array[a]);
 		a++;
-		if (a == min_value(b + 1, size))
+		if (a == size)
 			return (-1);
 	}
 
